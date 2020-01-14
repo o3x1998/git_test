dd75fabf5f20629353e3d0872ab4d221a82a47cf
o3x1998 <munjeong0719@gmail.com>
2020-01-14 14:37:03 +0900

    my second commit

--- testClass.java
+++ testClass.java
@@ -2,7 +2,7 @@ package test;
 
 public class testClass {
 
-    public void static main (String[] args) {
+    public static void main (String[] args) {
         System.out.println("Hello!");
     }
 }
